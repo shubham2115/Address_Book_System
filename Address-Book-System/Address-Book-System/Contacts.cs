beddc3e69b980df0e4543d6e79de45da30f1d7dd
Shubham <s.shrikhande1@gmail.com>
2022-02-04 23:22:15 +0530

    Welcome to AddressBook

---
+++ Address-Book-System/Address-Book-System/Contacts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address_Book_System
+{
+    public class Contacts
+    {
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNunmber { get; set; }
+        public string eMail { get; set; }
+    }
+}
